d155d20aa68ec72f947d8e39e5b43818a898ab4f
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-01-29 15:27:37 +0900

    add base logic

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kaepa3/zipper
+
+go 1.15
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/naoina/go-stringutil v0.1.0 // indirect
+	github.com/naoina/toml v0.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+	conf := initConfig()
+	fmt.Println(conf)
+	zf, _ := os.Create(conf.OutputName)
+	defer zf.Close()
+	compress(zf, conf.Files)
+}
+
+type Config struct {
+	OutputName string
+	Files      []string
+}
+
+func initConfig() *Config {
+	c := Config{}
+	toml.DecodeFile("./config.toml", &c)
+	return &c
+
+}
+
+func compress(buf io.Writer, files []string) {
+	w := zip.NewWriter(buf)
+
+	for _, file := range files {
+		info, _ := os.Stat(file)
+
+		hdr, _ := zip.FileInfoHeader(info)
+		hdr.Name = "files/" + file
+		f, err := w.CreateHeader(hdr)
+		if err != nil {
+			panic(err)
+		}
+
+		body, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		f.Write(body)
+	}
+
+	w.Close()
+
+	return
+}
+
+func save(b *bytes.Buffer) error {
+	zf, err := os.Create("sample.zip")
+	if err != nil {
+		return err
+	}
+	zf.Write(b.Bytes())
+	zf.Close()
+	return nil
+}
